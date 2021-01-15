a5d53a9c5fcabd472e5adee486ad1b4051db77c3
taitus <sebastia.roig@gmail.com>
2021-01-15 12:07:41 +0100

    Manage goals icons status when add or remove related targets
    Whenever there is a tag related to Goal, either the Goal itself or a Target, the icon will
    be "checked".
    When there is no related Goal or Target it will no longer be marked as checked.

--- app/assets/javascripts/sdg/related_list_selector.js
+++ app/assets/javascripts/sdg/related_list_selector.js
@@ -7,6 +7,18 @@
         suggestions: $(".sdg-related-list-selector .input").data("suggestions-list"),
         classes: $(".sdg-related-list-selector .input").data("class-list"),
         whiteList: true,
+        afterRemove: function(value) {
+          var values = $(amsifySuggestags.selector).val().split(",");
+          var parent_values = jQuery.map(values, function(v) {
+            return (v.split(".")[0]);
+          });
+          if (parent_values.indexOf(value.toString().split(".")[0]) === -1) {
+            $("#goal_" + value.split(".")[0]).removeClass("checked");
+          }
+        },
+        afterAdd: function(value) {
+          $("#goal_" + value.toString().split(".")[0]).addClass("checked");
+        },
       });
       amsifySuggestags.classes.focus = ".sdg-related-list-focus";
       amsifySuggestags.classes.sTagsInput = ".sdg-related-list-selector-input";
