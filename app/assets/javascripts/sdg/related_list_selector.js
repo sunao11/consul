1d7eb440976edeb5358d5fe6c31e6142a94efad3
taitus <sebastia.roig@gmail.com>
2021-01-15 10:51:07 +0100

    Add whiteList setting
    With this setting to true, only can add tags related with suggestions.

--- app/assets/javascripts/sdg/related_list_selector.js
+++ app/assets/javascripts/sdg/related_list_selector.js
@@ -5,6 +5,7 @@
       var amsifySuggestags = new AmsifySuggestags($(".sdg-related-list-selector .input"));
       amsifySuggestags._settings({
         suggestions: $(".sdg-related-list-selector .input").data("suggestions-list"),
+        whiteList: true,
       });
       amsifySuggestags.classes.focus = ".sdg-related-list-focus";
       amsifySuggestags.classes.sTagsInput = ".sdg-related-list-selector-input";
